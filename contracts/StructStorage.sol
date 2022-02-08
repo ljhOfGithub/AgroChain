b9f500de6a6209298352419e2eb64706ecb5e615
ljhOfGithub <42089575+ljhOfGithub@users.noreply.github.com>
2022-02-09 00:59:15 +0800

    annotation

--- contracts/StructStorage.sol
+++ contracts/StructStorage.sol
@@ -36,32 +36,32 @@ struct farmer {
     bytes32 crop;
     uint256 contact;
     uint quantity;
-    uint exprice;
+    uint exprice;//期望价格
 }
 
 struct lot {
 
-    bytes lotno;
+    bytes lotno;//批号
     bytes grade;
-    uint mrp;
+    uint mrp;//物资需求计划
     bytes32 testdate;
-    bytes32 expdate;
+    bytes32 expdate;//期望日期
 }
 
 address public tester;
 address owner;
 
-mapping (bytes => farmer) f1;
+mapping (bytes => farmer) f1;//农民字节码到农民结构体的映射
 farmer[] public fm;
 
-mapping (bytes => lot) l1;
+mapping (bytes => lot) l1;//批次字节码到批次结构体的映射
 lot[] public l;
 
 
 
 function produce(bytes memory id, bytes32 name, bytes32 loc, bytes32 cr, uint256 con, uint q, uint pr) public{
                
-        StructStorage.farmer memory fnew = farmer(id,name,loc,cr,con,q,pr);
+        StructStorage.farmer memory fnew = farmer(id,name,loc,cr,con,q,pr);//注册一个新农民
         f1[id] = fnew;
         fm.push(fnew);
         s++;
@@ -69,9 +69,9 @@ function produce(bytes memory id, bytes32 name, bytes32 loc, bytes32 cr, uint256
 }
     
  function getproduce(bytes memory j) public view returns(bytes memory,bytes32,bytes32,bytes32,uint256,uint,uint) {
-        return (f1[j].fid,f1[j].fname,f1[j].loc,f1[j].crop,f1[j].contact,f1[j].quantity,f1[j].exprice);
+        return (f1[j].fid,f1[j].fname,f1[j].loc,f1[j].crop,f1[j].contact,f1[j].quantity,f1[j].exprice);//农民生产
     }
- function quality(bytes memory ll, bytes memory g, uint256 p, bytes32 tt, bytes32 e) public{
+ function quality(bytes memory ll, bytes memory g, uint256 p, bytes32 tt, bytes32 e) public{//批次信息
     
         StructStorage.lot memory lnew=lot(ll,g,p,tt,e);
         l1[ll]=lnew;
@@ -79,7 +79,7 @@ function produce(bytes memory id, bytes32 name, bytes32 loc, bytes32 cr, uint256
         t++;
   
  }  
- function getquality(bytes memory k) public view returns(bytes memory,bytes memory,uint,bytes32,bytes32) {
+ function getquality(bytes memory k) public view returns(bytes memory,bytes memory,uint,bytes32,bytes32) {//获取批次质量
      return(l1[k].lotno,l1[k].grade,l1[k].mrp,l1[k].testdate,l1[k].expdate);
      
  }
